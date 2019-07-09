d6bb8da33e3aafd4c7a5ad079b5864a8506884e3
SarahUriarte <uri.arte08@gmail.com>
2019-07-09 16:59:36 -0600

    Ventana de Préstamo expiró

---
+++ BiciTECProject/app/src/main/java/com/example/bicitec_project/LoanExpired.java
@@ -0,0 +1,13 @@
+package com.example.bicitec_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoanExpired extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_expired);
+    }
+}
